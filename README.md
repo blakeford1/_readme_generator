b015cbaa1158b2d56a0e9e76d2bf22a097f25bd2
Blake Ford <blakeaford88@gmail.com>
2020-05-19 21:11:43 -0600

    test2

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-
-      Project Title - ReadMe.md Generator
-      Description - Using the Command line you are able to make a descriptive readme with greather ease than using github alone.
-      Table of Contents - N/A
-      Installation - For this you just need to be using the command line and use npm install
-      Usage - this
-      License - that
-      Contributing - ma a
-      Tests - ererwerw efwer f 
-      Question1 - lorem	
+(data, image, email) => {
+      console.log(data);
+      return `
+  # ${data.title}
+  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Description
+    ${data.description}
+   ## Table of Contents
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+    *[Contributing](#contributing)
+    *[Tests](#tests)
+    *[Questions](#questions)
     
+  ## Installation
+  To install necessary dependencies, run the following command:
+      ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  This project is licensed under the ${data.usage} license.
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  To run tests, run the following command:
+      ${data.tests}
+  ## Questions
+  <img src="${image}" alt="avatar" style="border-radius: 16px" width="30"/>
+  If you have any questions about the repo, open an issue or contact@
+  [${data.username}]${email}
+    `;
+    }
--- index.js
+++ index.js
@@ -1,76 +1,114 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-inquirer
-  .prompt([
+function readMeQuestions() {
+  return inquirer.prompt([
     {
-      type: "Projet Title",
-      message: "Project Title?",
+      type: "input",
       name: "title",
+      message: "Please write a title for your project",
     },
     {
-      type: "Description",
-      message: "Description?",
+      type: "input",
       name: "description",
+      message: "Please write a short description of your project",
     },
     {
-      type: "Table of Contents",
-      message: "Table of Contents?",
-      name: "contents",
-    },
-    {
-      type: "Installation",
-      message: "Installation?",
-      name: "installation",
-    },
-    {
-      type: "Usage",
-      message: "Usage?",
-      name: "usage",
-    },
-    {
-      type: "License",
-      message: "License?",
+      type: "input",
       name: "license",
+      message: "What kind of license should your project have?",
     },
     {
-      type: "Contributing",
-      message: "Who is Contributing?",
-      name: "contributing",
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependecies?",
     },
     {
-      type: "Tests",
-      message: "Tests?",
+      type: "input",
       name: "tests",
+      message: "What command should be run to run tests?",
     },
     {
-      type: "Question",
-      message: "User GitHub profile picture?",
-      name: "question1",
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
     },
     {
-      type: "Question",
-      message: "User GitHub email?",
-      name: "question2",
+      type: "input",
+      name: "contributing",
+      message:
+        "What does the user need to know about contributing to the repo?",
     },
-  ])
+    {
+      type: "input",
+      name: "username",
+      message: "What is your Github Username?",
+    },
+  ]);
+}
 
-  .then(function (answers) {
-    console.log("answers ??", answers);
+var user = (answers) => {
+  var queryUrl = `https://api.github.com/users/${answers.username}`;
+  return axios.get(queryUrl);
+};
 
-    var readMeText = `
-      Project Title - ${answers.title}
-      Description - ${answers.description}
-      Table of Contents - ${answers.contents}
-      Installation - ${answers.installation}
-      Usage - ${answers.usage}
-      License - ${answers.license}
-      Contributing - ${answers.contributing}
-      Tests - ${answers.tests}
-      Question1 - ${answers.question1}
+const readMeText = (answers, image, email) => {
+  console.log(answers);
+  return `
+ 
+  # ${answers.title}
+  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Description
+    ${answers.description}
+   ## Table of Contents
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+    *[Contributing](#contributing)
+    *[Tests](#tests)
+    *[Questions](#questions)
+    
+  ## Installation
+  To install necessary dependencies, run the following command:
+      ${answers.installation}
+  ## Usage
+  ${answers.usage}
+  ## License
+  This project is licensed under the ${answers.usage} license.
+  ## Contributing
+  ${answers.contributing}
+  ## Tests
+  To run tests, run the following command:
+      ${answers.tests}
+  ## Questions
+  <img src="${image}" alt="avatar" style="border-radius: 16px" width="30"/>
+  If you have any questions about the repo, open an issue or contact@
+  [${answers.username}]${email}
     `;
+};
 
-    fs.writeFile("readMe.md", readMeText, function (err) {
-      console.log("err ??", err);
-    });
-  });
+async function init() {
+  console.log("Generating README");
+  try {
+    const answers = await readMeQuestions();
+
+    const res = await user(answers);
+
+    const md = readMeText(
+      answers,
+      res.answers.avatar_url,
+      res.answers.html_url
+    );
+
+    await writeFileAsync("README.md", md);
+
+    console.log("Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
