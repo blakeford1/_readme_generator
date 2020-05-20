47f236998f260239e4a514a544c6eed766188ae5
Blake Ford <blakeaford88@gmail.com>
2020-05-19 21:48:18 -0600

    she looks great now

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
 
 # readMe_Generator
-![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+(![AppVeyor](https://img.shields.io/appveyor/build/blakeford1/_readme_generator))
 ## Description
-  Creates a readMe using the command line
- ## Table of Contents
-  *[Installation](#installation)
-  *[Usage](#usage)
-  *[License](#license)
-  *[Contributing](#contributing)
-  *[Tests](#tests)
-  *[Questions](#questions)
+Creates a readMe from the command line
+## Table of Contents
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+ *[Contributing](#contributing)
+ *[Tests](#tests)
+*[Questions](#questions)
   
 ## Installation
 To install necessary dependencies, run the following command:
-    npm i
+npm i
 ## Usage
-It requires use of the command line
+You will use the command line
 ## License
-This project is licensed under the It requires use of the command line license.
+This project is licensed under the You will use the command line license.
 ## Contributing
-Email me for more information
+email me for info.
 ## Tests
 To run tests, run the following command:
-    npm test
+npm test
 ## Questions
-<img src="https://avatars2.githubusercontent.com/u/61363728?v=4" alt="avatar" style="border-radius: 16px" width="30"/>
+<img src="https://avatars2.githubusercontent.com/u/61363728?v=4" alt="avatar" style="border-radius: 18px" width="35"/>
 If you have any questions about the repo, open an issue or contact@
 [blakeford1]https://github.com/blakeford1
   
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-const inquirer = require("inquirer");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function repoQuestions() {
+function questions() {
   return inquirer.prompt([
     {
       type: "input",
@@ -51,29 +51,29 @@ function repoQuestions() {
   ]);
 }
 
-var getName = (data) => {
+var user = (data) => {
   var queryUrl = `https://api.github.com/users/${data.username}`;
   return axios.get(queryUrl);
 };
 
-const generateMarkdown = (data, image, email) => {
+const readMeTxt = (data, image, email) => {
   console.log(data);
   return `
 # ${data.title}
-![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+(![AppVeyor](https://img.shields.io/appveyor/build/blakeford1/_readme_generator))
 ## Description
-  ${data.description}
- ## Table of Contents
-  *[Installation](#installation)
-  *[Usage](#usage)
-  *[License](#license)
-  *[Contributing](#contributing)
-  *[Tests](#tests)
-  *[Questions](#questions)
+${data.description}
+## Table of Contents
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+ *[Contributing](#contributing)
+ *[Tests](#tests)
+*[Questions](#questions)
   
 ## Installation
 To install necessary dependencies, run the following command:
-    ${data.installation}
+${data.installation}
 ## Usage
 ${data.usage}
 ## License
@@ -82,9 +82,9 @@ This project is licensed under the ${data.usage} license.
 ${data.contributing}
 ## Tests
 To run tests, run the following command:
-    ${data.tests}
+${data.tests}
 ## Questions
-<img src="${image}" alt="avatar" style="border-radius: 16px" width="30"/>
+<img src="${image}" alt="avatar" style="border-radius: 18px" width="35"/>
 If you have any questions about the repo, open an issue or contact@
 [${data.username}]${email}
   `;
@@ -93,13 +93,13 @@ If you have any questions about the repo, open an issue or contact@
 async function init() {
   console.log("Generating README");
   try {
-    const data = await repoQuestions();
+    let data = await questions();
 
-    const res = await getName(data);
+    let res = await user(data);
 
-    const md = generateMarkdown(data, res.data.avatar_url, res.data.html_url);
+    let markdown = readMeTxt(data, res.data.avatar_url, res.data.html_url);
 
-    await writeFileAsync("README.md", md);
+    await writeFileAsync("README.md", markdown);
 
     console.log("Successfully wrote to README.md");
   } catch (err) {
